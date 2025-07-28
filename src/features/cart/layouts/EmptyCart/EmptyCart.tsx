f2b15e75d4e64a2dab4b0e50fdf7944a1feff661
millisabel <elena.kozlova.job@gmail.com>
2025-07-28 13:22:04 +0200

    Remove GoToShop button from EmptyCart component for a cleaner layout and improved user experience.

--- src/features/cart/layouts/EmptyCart/EmptyCart.tsx
+++ src/features/cart/layouts/EmptyCart/EmptyCart.tsx
@@ -1,12 +1,10 @@
 import ContentLayout from '@/components/patterns/ContentLayout';
 import CartIcon from '@/components/shared/Icon/CartIcon';
-import GoToShop from '@/components/shared/Button/GoToShop';
 
 const EmptyCart = () => (
   <ContentLayout variant="flexCol" align="center" className="py-20">
     <CartIcon className="w-32 h-32 text-primary opacity-60 mb-8" />
     <p className="text-xl text-neutral-500 mb-6">Your cart is empty.</p>
-    <GoToShop children="Go to Shop" />
   </ContentLayout>
 );
 
